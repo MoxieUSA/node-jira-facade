e8dd2419aacba62be039cc393de75e2e266874bc
Keith Morris <keith.morris@moxieusa.com>
2015-08-17 20:49:21 -0400

    Stub out unit testing

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Keith Morris on 8/17/15.
+ */
+var expect = require('chai').expect;
+
+describe('Test jira-facade', function(){
+	describe('Stub out unit test', function(){
+		it('true should equal true', function(){
+			expect(true).to.be.true;
+		});
+	});
+});
